d5ad9eb369b24763dd48eb4a28ef085c69906c38
AdelFenin <adel.fenin@gmail.com>
2022-08-10 13:48:26 +0300

    first commit

---
+++ lib/api/getCertificate.d.ts
@@ -0,0 +1,10 @@
+import { Certificate } from "./certificate";
+/**
+ * Возвращает сертификат по отпечатку
+ *
+ * @param thumbprint - отпечаток сертификата
+ * @returns сертификат
+ */
+export declare const getCertificate: (
+  thumbprint: string
+) => Promise<Certificate>;
